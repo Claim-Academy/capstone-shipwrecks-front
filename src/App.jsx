e3cb6b03c3699f8e3388b1de00f0667ffd9f7462
Manav Misra <manavm@visionwebsoft.com>
2023-02-13 14:08:45 -0600

    feat: WIP render example map

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import ShipwreckForm from "./components/shipwreck-form";
 import { shipwrecksApi } from "./services";
 
@@ -22,10 +23,30 @@ function App() {
   }, []);
 
   return (
-    <ShipwreckForm
-      value={selectedShipwreckType}
-      setValue={setSelectedShipwreckType}
-    />
+    <main>
+      <ShipwreckForm
+        value={selectedShipwreckType}
+        setValue={setSelectedShipwreckType}
+      />
+      <MapContainer
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{
+          height: "100vh",
+        }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </main>
   );
 }
 
