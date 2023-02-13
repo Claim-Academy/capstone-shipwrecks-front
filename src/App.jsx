bf417c7e653c39ed7e8df4a40e13730f9e7e4ddd
Manav Misra <manavm@visionwebsoft.com>
2023-02-13 11:34:21 -0600

    Log coordinates from database

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
+import { shipwrecksApi } from "./services";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    shipwrecksApi
+      .index()
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+  }, []);
+
+  return (
+    <ul>
+      {/* Need braces around JS code in JSX */}
+      {data.map((shipwreck) => {
+        return (
+          <li key={shipwreck.id}>
+            {shipwreck.latdec}, {shipwreck.londec}
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default App;
