ad9fc0af9f7b2a42bd9985d7003b76efa7d8a95e
Manav Misra <manavm@visionwebsoft.com>
2023-02-13 15:22:34 -0600

    Resolve Marker in Leaflet with custom icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { Icon } from "leaflet";
 import { useEffect, useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
+import { Marker } from "react-leaflet/Marker";
 import ShipwreckForm from "./components/shipwreck-form";
+import marker from "./pindrop.svg";
 import { shipwrecksApi } from "./services";
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
 
   // This is the state that will be updated when the user selects a shipwreck type
   const [selectedShipwreckType, setSelectedShipwreckType] = useState(
@@ -22,30 +25,37 @@ function App() {
       });
   }, []);
 
+  const icon = new Icon({
+    iconUrl: marker,
+    iconSize: [24, 24],
+  });
+
   return (
     <main>
       <ShipwreckForm
         value={selectedShipwreckType}
         setValue={setSelectedShipwreckType}
       />
-      <MapContainer
-        center={[51.505, -0.09]}
-        zoom={13}
-        scrollWheelZoom={false}
-        style={{
-          height: "100vh",
-        }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      {data && (
+        <MapContainer
+          center={[-78.8790131, 9.5574865]}
+          zoom={3}
+          scrollWheelZoom={false}
+          style={{
+            height: "100vh",
+          }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker
+            position={[data[0].latdec, data[0].londec]}
+            icon={icon}
+            sx={{ fill: "red", stroke: "red", strokeWidth: 2, color: "red" }}
+          ></Marker>
+        </MapContainer>
+      )}
     </main>
   );
 }
