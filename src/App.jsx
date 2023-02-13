dc26b0a48a39f66e464860fcfd8626a2c5b9d99d
Manav Misra <manavm@visionwebsoft.com>
2023-02-13 12:04:28 -0600

    refactor: move ShipWreckForm to own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import { Button } from "@mui/material";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import Select from "@mui/material/Select";
 import { useEffect, useState } from "react";
+import ShipwreckForm from "./components/shipwreck-form";
 import { shipwrecksApi } from "./services";
 
 function App() {
@@ -25,26 +22,10 @@ function App() {
   }, []);
 
   return (
-    <form className="flex flex-col gap-y-4 px-4 pt-4">
-      <Select
-        onChange={(event) => {
-          setSelectedShipwreckType(event.target.value);
-        }}
-        label="Shipwreck Type"
-        labelId="shipwreck-type-label"
-        id="shipwreck-type"
-        value={selectedShipwreckType}
-      >
-        <InputLabel id="shipwreck-type">Shipwreck Type</InputLabel>
-        <MenuItem value="Wrecks - Submerged, dangerous">Submerged</MenuItem>
-        <MenuItem value="Wrecks - Not Visible">Not Visible</MenuItem>
-        <MenuItem value="Wrecks - Submerged, nondangerous">Dangerous</MenuItem>
-      </Select>
-
-      <Button type="submit" variant="contained">
-        Submit
-      </Button>
-    </form>
+    <ShipwreckForm
+      value={selectedShipwreckType}
+      setValue={setSelectedShipwreckType}
+    />
   );
 }
 
