8fa6885498e137359e1d30b0410615d7e17d7aed
Manav Misra <manavm@visionwebsoft.com>
2023-02-13 11:54:37 -0600

    Render the Select and update it using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
 import { useEffect, useState } from "react";
 import { shipwrecksApi } from "./services";
 
 function App() {
   const [data, setData] = useState([]);
+
+  // This is the state that will be updated when the user selects a shipwreck type
+  const [selectedShipwreckType, setSelectedShipwreckType] = useState(
+    "Wrecks - Submerged, dangerous"
+  );
 
   useEffect(() => {
     shipwrecksApi
@@ -16,16 +24,20 @@ function App() {
   }, []);
 
   return (
-    <ul>
-      {/* Need braces around JS code in JSX */}
-      {data.map((shipwreck) => {
-        return (
-          <li key={shipwreck._id}>
-            {shipwreck.latdec}, {shipwreck.londec}
-          </li>
-        );
-      })}
-    </ul>
+    <Select
+      onChange={(event) => {
+        setSelectedShipwreckType(event.target.value);
+      }}
+      label="Shipwreck Type"
+      labelId="shipwreck-type-label"
+      id="shipwreck-type"
+      value={selectedShipwreckType}
+    >
+      <InputLabel id="shipwreck-type">Shipwreck Type</InputLabel>
+      <MenuItem value="Wrecks - Submerged, dangerous">Submerged</MenuItem>
+      <MenuItem value="Wrecks - Not Visible">Not Visible</MenuItem>
+      <MenuItem value="Wrecks - Submerged, nondangerous">Dangerous</MenuItem>
+    </Select>
   );
 }
 
